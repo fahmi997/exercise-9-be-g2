8a4240105eeb630319effe7c592c29a44504181d
fahmi997 <aardiansyah.fahmii@gmail.com>
2023-11-01 15:34:16 +0700

    feat: add tweets model

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const PORT = process.env.PORT || 5000;
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { tweetRouter } = require('./routers');
 
 app.use(cors());
 app.use(express.json());
@@ -11,7 +12,9 @@ app.get('/', (req, res) => {
     return res.status(200).send('API is up and running')
 });
 
+//* ROUTES
+app.use('/tweet', tweetRouter)
 
 app.listen(PORT, () => {
-    console.log('server listening on port',PORT);
+    console.log('server listening on port', PORT);
 });
--- routers/index.js
+++ routers/index.js
@@ -1,1 +1,5 @@
-module.exports = {};
+const tweetRouter = require('./tweet')
+
+module.exports = {
+    tweetRouter
+};
